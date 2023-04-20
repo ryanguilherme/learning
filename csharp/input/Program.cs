c731f88a3c598f1faaa8aff141a18f23dad62892
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-04-20 15:29:38 -0300

    teste

--- csharp/input/Program.cs
+++ csharp/input/Program.cs
@@ -1,6 +1,17 @@
-﻿Console.WriteLine("type your name: ");
-var name = Console.ReadLine();
-var date = DateTime.Now;
-Console.WriteLine($"{Environment.NewLine}hello, {name}, on {date:d} at {date:t}!");
-Console.WriteLine($"{Environment.NewLine}Press any key to exit...");
-Console.ReadKey(true);
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("type your name: ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}hello, {name}, on {date:d} at {date:t}!");
+            Console.WriteLine($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
