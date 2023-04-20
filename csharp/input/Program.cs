fafbc9c31edd919667a442f3b015bc414d0cb60a
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-04-20 15:26:19 -0300

    input csharp

---
+++ csharp/input/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("type your name: ");
+var name = Console.ReadLine();
+var date = DateTime.Now;
+Console.WriteLine($"{Environment.NewLine}hello, {name}, on {date:d} at {date:t}!");
+Console.WriteLine($"{Environment.NewLine}Press any key to exit...");
+Console.ReadKey(true);
