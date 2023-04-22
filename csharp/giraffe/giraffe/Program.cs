671fdec44940552037d06f7f02c46cc62de3ab1a
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-04-22 14:39:51 -0300

    learning chsharp

--- csharp/giraffe/giraffe/Program.cs
+++ csharp/giraffe/giraffe/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-
+            Console.Write("teste");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            string tome = "teste formatacoes disponiveis";
+            Console.WriteLine(tome.First() + tome.ToUpper() + tome.ToLower());
         }
     }
 }
