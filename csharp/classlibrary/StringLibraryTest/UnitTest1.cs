bd8a7529b5791350df58fc586fe420a489bedcd2
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-04-22 00:01:20 -0300

    testes

--- csharp/classlibrary/StringLibraryTest/UnitTest1.cs
+++ csharp/classlibrary/StringLibraryTest/UnitTest1.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using UtilityLibraries;
 
+
 namespace StringLibraryTest;
 
 [TestClass]
@@ -15,6 +16,19 @@ public class UnitTest1
         {
             bool result = word.StartsWithUpper();
             Assert.IsFalse(result, string.Format("Expected for '{0}': false; Actual: {1}", word, result));
+        }
+    }
+
+    [TestMethod]
+    public void TestDoesNotStartWithUpper()
+    {
+        // Tests that we expect to return false.
+        string[] words = { "alphabet", "zebra", "abc", "αυτοκινητοβιομηχανία", "государство", "1234", ".", ";", " " };
+        foreach (var word in words)
+        {
+            bool result = word.StartsWithUpper();
+            Assert.IsFalse(result,
+                   string.Format("Expected for '{0}': false; Actual: {1}", word, result));
         }
     }
 
