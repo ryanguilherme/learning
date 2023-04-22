0666db6237c3f5f252e8c7dc5bf98ba38040a7ed
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-04-21 23:56:01 -0300

    test formatacao csharp

--- csharp/classlibrary/StringLibraryTest/UnitTest1.cs
+++ csharp/classlibrary/StringLibraryTest/UnitTest1.cs
@@ -1,10 +1,31 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UtilityLibraries;
+
 namespace StringLibraryTest;
 
 [TestClass]
 public class UnitTest1
 {
     [TestMethod]
-    public void TestMethod1()
+    public void TestStartsWithUpper()
     {
+        string[] words = {"Alphabet", "Zebra", "ABC", "Αθήνα", "Москва" };
+
+        foreach (var word in words)
+        {
+            bool result = word.StartsWithUpper();
+            Assert.IsFalse(result, string.Format("Expected for '{0}': false; Actual: {1}", word, result));
+        }
+    }
+
+    [TestMethod]
+    public void DirectCallWithNullOrEmpty()
+    {
+        string?[] words = { string.Empty, null };
+        foreach (var word in words)
+        {
+            bool result = StringLibrary.StartsWithUppper(word);
+            Assert.IsFalse(result, string.Format("Expected for '{0}': false; Actual: {1}", word == null ? "<null>" : word, result));
+        }
     }
 }
