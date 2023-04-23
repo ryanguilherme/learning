99e51a7da91dac440a343087f276061ac81730b2
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-04-23 00:07:40 -0300

    calculadora funcionando

--- csharp/calculator/calculator/Program.cs
+++ csharp/calculator/calculator/Program.cs
@@ -7,39 +7,40 @@
             string? option;
             while (true)
             {
-                Console.WriteLine("Escolha uma operacao: +, -, *, /");
+                Console.WriteLine();
+                Console.WriteLine("Escolha uma operacao: +, -, *, /, ou ENTER para encerrar");
                 option = Console.ReadLine();
                 if (option == "+")
                 {
                     Console.Write("Digite um numero: ");
-                    int a = Console.Read();
+                    int a = Convert.ToInt32(Console.ReadLine());
                     Console.Write("Digite outro numero para somar: ");
-                    int b = Console.Read();
+                    int b = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine();
                     Console.WriteLine($"{a} + {b} = {a+b}");
                 }
                 else if (option == "-")
                 {
                     Console.Write("Digite um numero: ");
-                    int a = Console.Read();
+                    int a = Convert.ToInt32(Console.ReadLine());
                     Console.Write("Digite outro numero para subtrair: ");
-                    int b = Console.Read();
+                    int b = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine($"{a} - {b} = {a - b}");
                 }
                 else if (option == "*")
                 {
                     Console.Write("Digite um numero: ");
-                    int a = Console.Read();
+                    int a = Convert.ToInt32(Console.ReadLine());
                     Console.Write("Digite outro numero para multiplicar");
-                    int b = Console.Read();
+                    int b = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine($"{a} * {b} = {a * b}");
                 }
                 else if (option == "/")
                 {
                     Console.Write("Digite um numero: ");
-                    int a = Console.Read();
+                    int a = Convert.ToInt32(Console.ReadLine());
                     Console.Write("Digite outro numero para dividir: ");
-                    int b = Console.Read();
+                    int b = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine($"{a} / {b} = {a / b}");
                 }
                 else
