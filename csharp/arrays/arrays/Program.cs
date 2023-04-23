87b6127ae32601f6261ba4fbf49c41ac9953df39
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-04-23 00:12:43 -0300

    arrays

---
+++ csharp/arrays/arrays/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace arrays
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, };
+            foreach(int i in array)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine($"array length = {array.Length}" );
+            Console.WriteLine($"array first  = {array.First()}");
+            Console.WriteLine($"array test = {array[5]}");
+        }
+    }
+}
