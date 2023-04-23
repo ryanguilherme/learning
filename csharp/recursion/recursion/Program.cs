23d720b59700437cc93677e638d35b2bf8cb727a
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-04-23 00:25:42 -0300

    programa recursivo para elevar um numero

---
+++ csharp/recursion/recursion/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace recursion
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Digite um numero: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o numero de vezes que voce quer eleva-lo: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            int res = pow(n, m);
+            Console.WriteLine($"{n} elevado a {m} = {res}");
+        }
+
+        public static int pow(int n, int m)
+        {
+            if (m == 0)
+            {
+                return 1;
+            }
+            return n * pow(n, m - 1);
+        }
+    }
+}
